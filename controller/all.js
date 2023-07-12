01aa13e0d65936fde564b0866d061d9be14621a6
sundarasekar <sundarasekar01@gmail.com>
2023-07-12 18:55:46 +0530

    cart

--- controller/all.js
+++ controller/all.js
@@ -18,11 +18,30 @@ const logout = (req,res)=>{
     res.render('logout')
 }
 
+const profile = (req,res)=>{
+    res.render('profile')
+}
+
+const wishlist = (req,res)=>{
+    res.render('wishlist')
+}
+
+const product = (req,res)=>{
+    res.render('product')
+}
+
+const cart = (req,res)=>{
+    res.render('cart')
+}
 
 
 module.exports={
     landing,
     login,
     register,
-    logout
+    logout,
+    profile,
+    product,
+    wishlist,
+    cart
 }
--- router/allRoute.js
+++ router/allRoute.js
@@ -9,5 +9,9 @@ route.get('/',con.landing)
 route.get('/login',con.login)
 route.get('/register',con.register)
 route.get('/logout',con.logout)
+route.get('/product',con.logout)
+route.get('/profile',con.logout)
+route.get('/wishlist',con.logout)
+route.get('/cart',con.logout)
 
 module.exports=route;
