fbc93fb03c369d3d598608d1d65274893ad72477
sundarasekar <sundarasekar01@gmail.com>
2024-07-04 15:48:02 +0530

    error handling

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app =express();
+const errorMiddleware = require("./middlewares/error")
 
 app.use(express.json());
 
@@ -7,5 +8,6 @@ const products = require('./routes/productRoutes');
 app.use('/api/v1/' , products);
 
 
+app.use(errorMiddleware);
 
 module.exports = app;
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,10 +1,11 @@
 const Product = require("../models/productModel");
+const ErrorHandler = require('../utils/errorHandler');
 
 
 // Get Product - api/v1/products
 
 exports.getProducts = async (req, res, next) => {
-  try {
+
     const products = await Product.find();
 
     res.status(200).json({ 
@@ -14,20 +15,14 @@ exports.getProducts = async (req, res, next) => {
       products,
     });
 
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: "Server error",
-      error: error.message,
-    });
-  }
+
 };
 
 
 // Add Product - api/v1/products/add
 
 exports.addProduct = async (req, res, next) => {
-  try {
+
     const product = await Product.create(req.body);
 
     res.status(201).json({
@@ -35,27 +30,17 @@ exports.addProduct = async (req, res, next) => {
       product,
     });
 
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: "Server error",
-      error: error.message,
-    });
-  }
 };
 
 
 // Get Single Product - api/v1/products/:id
 
 exports.getSingleProduct = async (req, res, next) => {
-  try {
+  
     const product = await Product.findById(req.params.id);
 
     if (!product) {
-      return res.status(404).json({
-        success: false,
-        message: "Product not found",
-      });
+      return next(new ErrorHandler('Product not found',404));
     }
 
     res.status(201).json({
@@ -63,27 +48,17 @@ exports.getSingleProduct = async (req, res, next) => {
       product,
     });
 
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: "Server error",
-      error: error.message,
-    });
-  }
 };
 
 
 // Update Product - api/v1/products/:id
 
 exports.updateProduct = async (req, res, next) => {
-  try {
+ 
     let product = await Product.findById(req.params.id);
 
     if (!product) {
-      return res.status(404).json({
-        success: false,
-        message: "Product not found",
-      });
+      return next(new ErrorHandler('Product not found',404));
     }
 
     product = await Product.findByIdAndUpdate(req.params.id, req.body, {
@@ -96,20 +71,14 @@ exports.updateProduct = async (req, res, next) => {
       product,
     });
 
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: "Server error",
-      error: error.message,
-    });
-  }
+
 };
 
 
 // Delete Product - api/v1/products/:id
 
 exports.deleteProduct = async (req, res, next) => {
-  try {
+
     const product = await Product.findByIdAndDelete(req.params.id);
 
     res.status(200).json({
@@ -117,7 +86,5 @@ exports.deleteProduct = async (req, res, next) => {
       product,
     });
 
-  } catch (error) {
-    console.log(error.message);
-  }
+  
 };
